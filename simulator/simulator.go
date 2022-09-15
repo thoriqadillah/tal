3177c31751f71edfe54d7fc6439b8122189331fe
thoriqadillah <thoriqadillah59@gmail.com>
2022-09-15 23:40:52 +0700

    fix index out of bound

--- simulator/simulator.go
+++ simulator/simulator.go
@@ -20,18 +20,29 @@ func (s *Simulator) Rule(p1 []entity.Particle, p2 []entity.Particle, gravity flo
 			dx := p1[i].Position.X - p2[j].Position.X
 			dy := p1[i].Position.Y - p2[j].Position.Y
 			distance := math.Sqrt(float64(dx*dx + dy*dy))
-			if distance > 0 {
+			if distance > 0 && distance < 80 {
 				F := gravity * 1 / distance
 				fx += F * float64(dx)
 				fy += F * float64(dy)
 			}
 		}
-		p1[i].Vx = (p1[i].Vx + fx)
-		p1[i].Vy = (p1[i].Vy + fy)
+		p1[i].Vx = (p1[i].Vx + fx) * 0.6
+		p1[i].Vy = (p1[i].Vy + fy) * 0.6
 
 		s.Canvas.Cells[p1[i].Position.Y][p1[i].Position.X] = " "
-		p1[i].Position.X += int(math.Round(p1[i].Vx))
-		p1[i].Position.Y += int(math.Round(p1[i].Vy))
+		x := (p1[i].Position.X + int(math.Round(p1[i].Vx))) % s.Canvas.Width
+		y := (p1[i].Position.Y + int(math.Round(p1[i].Vy))) % s.Canvas.Height
+
+		if x <= 0 {
+			x *= -1
+		}
+
+		if y <= 0 {
+			y *= -1
+		}
+
+		p1[i].Position.X = x
+		p1[i].Position.Y = y
 		s.Canvas.Cells[p1[i].Position.Y][p1[i].Position.X] = p1[i].Char
 	}
 }
