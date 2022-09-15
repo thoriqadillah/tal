91c41f21102c1a8b5000216e4fad715e20dfc28c
thoriqadillah <thoriqadillah59@gmail.com>
2022-09-15 21:00:31 +0700

    add comment

--- renderer/cursor.go
+++ renderer/cursor.go
@@ -4,6 +4,7 @@ import "fmt"
 
 type Cursor struct{}
 
+// set cursor of the terminal to top left corner to refresh and update the canvas
 func (c *Cursor) refresh() {
 	fmt.Print("\u001b[H")
 }
--- renderer/renderer.go
+++ renderer/renderer.go
@@ -9,7 +9,7 @@ import (
 func Render(simulator *simulator.Simulator) {
 	var cursor Cursor
 
-	time.Sleep(1000 * time.Millisecond / 30)
+	time.Sleep(1000 * time.Millisecond / 60) //this will make the renderer runs certain fps/s
 	simulator.Canvas.Render()
 	cursor.refresh()
 }
